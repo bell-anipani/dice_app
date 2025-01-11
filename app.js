912b58f92e065474ac54df312b7def017a7aa930
bell-anipani <skhedgehog@gmail.com>
2025-01-11 19:57:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 
 // Your web app's Firebase configuration
@@ -16,7 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const analytics = getAnalytics(app);
+const database = getDatabase(app); // Realtime Databaseを初期化
 
 // パスワード認証
 const correctPassword = "anipani"; // 設定したいパスワードをここに記載
